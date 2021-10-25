2e1b808f4253305a2710512c6977f2e7225a2aa3
Tibin-Saji <79278512+Tibin-Saji@users.noreply.github.com>
2021-10-25 18:32:13 +0300

    Update home.js

--- NewsApp/home.js
+++ NewsApp/home.js
@@ -18,9 +18,7 @@ function validate(){
         alert("You have left "+attempt+" attempt;");
 
         if( attempt == 0){
-            // document.getElementById("username").disabled = true;
-            // document.getElementById("password").disabled = true;
-            // document.getElementById("submit").disabled = true;
+            document.getElementById("submit").disabled = true;
         }
     }
 }
